d8d3b769e204384659f81ced30b5a6e714b19ddc
Joaquim Perez <jivkim@gmail.com>
2025-11-24 00:18:58 +0100

    remove csrfToken, not required

--- vue-app/stores/main.js
+++ vue-app/stores/main.js
@@ -21,7 +21,6 @@ export const useMainStore = defineStore("main", () => {
     return {
         appVersion: document.body.dataset.appVersion,
         appName: document.body.dataset.appName,
-        csrfToken: document.body.dataset.csrfToken,
         user,
         updateUser,
         flushSession,
