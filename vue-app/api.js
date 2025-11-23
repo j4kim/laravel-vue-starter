fe86f486a1dac91a63fabc728566341635895d2b
Joaquim Perez <jivkim@gmail.com>
2025-11-23 22:47:01 +0100

    refactor(api)

--- vue-app/api.js
+++ vue-app/api.js
@@ -1,37 +1,28 @@
 import axios from "axios";
 import { route } from "../vendor/tightenco/ziggy";
 
-export async function request(config) {
+export async function axiosRequest(config) {
     const response = await axios(config);
     return response.data;
 }
 
+export async function request(method, name, params = null, data = null) {
+    const url = route(name, params);
+    return await axiosRequest({ method, url, data });
+}
+
 export async function get(name, params = null) {
-    return await request({
-        method: "get",
-        url: route(name, params),
-    });
+    return await request("get", name, params);
 }
 
 export async function post(name, params = null, data = null) {
-    return await request({
-        method: "post",
-        url: route(name, params),
-        data,
-    });
+    return await request("post", name, params, data);
 }
 
 export async function put(name, params = null, data = null) {
-    return await request({
-        method: "put",
-        url: route(name, params),
-        data,
-    });
+    return await request("put", name, params, data);
 }
 
 export async function del(name, params = null) {
-    return await request({
-        method: "delete",
-        url: route(name, params),
-    });
+    return await request("delete", name, params);
 }
