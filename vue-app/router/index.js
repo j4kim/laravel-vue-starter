b5d520611baaf7fc914ca45abd24ac9f8c741e2d
Joaquim Perez <jivkim@gmail.com>
2025-11-24 00:51:33 +0100

    vue-router: use web history instead of hash (no # in url). In web.php, declare a fallback route that returns vue-app

--- vue-app/router/index.js
+++ vue-app/router/index.js
@@ -1,8 +1,9 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Profile from "../views/Profile.vue";
 import { useMainStore } from "../stores/main";
 import { redirectToLogin } from "../api";
+import { route } from "../../vendor/tightenco/ziggy";
 
 const routes = [
     {
@@ -21,13 +22,13 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 });
 
 router.beforeEach((to) => {
     if (to.meta?.requireAuth && !useMainStore().user) {
-        redirectToLogin("/" + to.href);
+        redirectToLogin(route("vue-app", "") + to.href);
         return false;
     }
 });
